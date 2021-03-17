2ff30a0a07a465ba1777b12b0e4e3448c3c21dd7
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2021-03-17 17:12:05 +0700

    Introduction class and object

--- src/github/io/haloppaing/classandobject/Main.java
+++ src/github/io/haloppaing/classandobject/Main.java
@@ -4,7 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
 
-        student.id = "1117041";
-        student
+        student.id        = "1117041";
+        student.firstname = "Alfiyanto";
+        student.lastname  = "Kondolele";
+        student.gender    = "Male";
+        student.religion  = "Christian";
+
+        System.out.println("ID        : " + student.id);
+        System.out.println("Firstname : " + student.firstname);
+        System.out.println("Lastname  : " + student.lastname);
+        System.out.println("Gender    : " + student.gender);
+        System.out.println("Religion  : " + student.religion);
     }
 }
