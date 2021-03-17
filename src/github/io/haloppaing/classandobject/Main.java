a7ba5300c22e9f3a8fe5653fe3037ca047007f03
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2021-03-17 17:07:44 +0700

    Initial commit

---
+++ src/github/io/haloppaing/classandobject/Main.java
@@ -0,0 +1,10 @@
+package github.io.haloppaing.classandobject;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.id = "1117041";
+        student
+    }
+}
---
+++ src/github/io/haloppaing/classandobject/Student.java
@@ -0,0 +1,9 @@
+package github.io.haloppaing.classandobject;
+
+public class Student {
+    String id;
+    String firstname;
+    String lastname;
+    String gender;
+    String religion;
+}
