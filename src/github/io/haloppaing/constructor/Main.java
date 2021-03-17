c9be159d45832d222c3700edcaab8e8e5b48ebfc
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2021-03-17 17:20:07 +0700

    Constructor

--- src/github/io/haloppaing/constructor/Main.java
+++ src/github/io/haloppaing/constructor/Main.java
@@ -2,6 +2,12 @@ package github.io.haloppaing.constructor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Contructor");
+        Student student = new Student("1117041", "Alfiyanto", "Kondolele", "Male", "Christian");
+
+        System.out.println("ID        : " + student.id);
+        System.out.println("Firstname : " + student.firstname);
+        System.out.println("Lastname  : " + student.lastname);
+        System.out.println("Gender    : " + student.gender);
+        System.out.println("Religion  : " + student.religion);
     }
 }
